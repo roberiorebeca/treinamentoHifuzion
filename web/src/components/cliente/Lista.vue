fe003c5f25f65317a41fad1c668e32046befb1a2
William Galleti <william.galleti@gmail.com>
2019-03-28 08:21:44 -0400

    Finalizando lista.

--- web/src/components/cliente/Lista.vue
+++ web/src/components/cliente/Lista.vue
@@ -1,3 +1,38 @@
 <template>
-  <h1>Lista de Cliente</h1>
+  <div id="hfz-lista-cliente">
+    <h1>{{ titulo }}</h1>
+    <v-data-table
+      :headers="headers"
+      :items="data"
+      class="elevation-1"
+    >
+      <template v-slot:items="props">
+        <td>{{ props.item.id }}</td>
+        <td>{{ props.item.nome }}</td>
+        <td>{{ props.item.fone }}</td>
+        <td>{{ props.item.email }}</td>
+        <td>{{ props.item.conta_display }}</td>
+      </template>
+    </v-data-table>
+  </div>
 </template>
+
+<script>
+
+export default {
+  props: {
+    titulo: {
+      type: String,
+      required: true
+    },
+    headers: {
+      type: Array,
+      default: () => []
+    },
+    data: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
